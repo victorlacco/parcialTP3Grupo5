38e5e53271dfd71f4a2b07e66acb26b30c988929
Diego Ponzo <diegoponzo@gmail.com>
2022-11-01 23:39:35 -0300

    Iniciacion del proyecto

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # parcialTP3Grupo5
+
+API: https://rickandmortyapi.com/api/character
+
--- app/src/main/java/ar/edu/ort/parcialtp3_g5/activities/MainActivity.kt
+++ app/src/main/java/ar/edu/ort/parcialtp3_g5/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package ar.edu.ort.parcialtp3_g5
+package ar.edu.ort.parcialtp3_g5.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ar.edu.ort.parcialtp3_g5.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
