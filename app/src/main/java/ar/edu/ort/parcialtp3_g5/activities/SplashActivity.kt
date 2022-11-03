c3332fb4061e19a73356e07c2b06b75edeb52c2c
Diego Ponzo <diegoponzo@gmail.com>
2022-11-03 01:17:59 -0300

    Se agregaro Splash Activity

---
+++ app/src/main/java/ar/edu/ort/parcialtp3_g5/activities/SplashActivity.kt
@@ -0,0 +1,30 @@
+package ar.edu.ort.parcialtp3_g5.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import ar.edu.ort.parcialtp3_g5.R
+import android.content.Intent
+import android.os.Looper
+import kotlinx.coroutines.delay
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SPLASH_TIME_OUT:Long = 2000
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //Handler(Looper.getMainLooper()).postDelayed(
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this,MainActivity::class.java))
+                //finish()
+            }
+            , SPLASH_TIME_OUT)
+    }
+
+}
